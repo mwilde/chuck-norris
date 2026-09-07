06610c628ae05b122057530c264bbcb9d7787bab
Moritz Wilde <moritz.wilde@kws.com>
2026-09-07 13:36:21 +0200

    feat: add ChuckNorrisShuffle, SurviveChuckNorris and ChuckNorrisCount to CollectionExtensions

--- tests/ChuckNorris.Extensions.Tests/Contexts/CollectionExtensionsContext.cs
+++ tests/ChuckNorris.Extensions.Tests/Contexts/CollectionExtensionsContext.cs
@@ -6,7 +6,15 @@ public sealed class CollectionExtensionsContext
 
     public List<string> OriginalList { get; set; } = [];
 
+    public List<string?> NullableList { get; set; } = [];
+
+    public IList<string>? ShuffledList { get; set; }
+
+    public IEnumerable<string>? FilteredList { get; set; }
+
     public string? PickedItem { get; set; }
+
+    public int CountResult { get; set; }
 
     public Exception? ThrownException { get; set; }
 }
--- tests/ChuckNorris.Extensions.Tests/Steps/CollectionExtensionsSteps.cs
+++ tests/ChuckNorris.Extensions.Tests/Steps/CollectionExtensionsSteps.cs
@@ -33,6 +33,18 @@ public class CollectionExtensionsSteps(CollectionExtensionsContext context)
         context.List = [];
     }
 
+    [Given("a nullable list with values {string}, null, {string}, null, {string}")]
+    public void GivenANullableListWithNulls(string v1, string v2, string v3)
+    {
+        context.NullableList = [v1, null, v2, null, v3];
+    }
+
+    [Given("a nullable list with values {string}, {string}, {string}")]
+    public void GivenANullableListWithValues(string v1, string v2, string v3)
+    {
+        context.NullableList = [v1, v2, v3];
+    }
+
     [When("I call SurvivedChuckNorris")]
     public void WhenICallSurvivedChuckNorris()
     {
@@ -50,6 +62,24 @@ public class CollectionExtensionsSteps(CollectionExtensionsContext context)
         {
             context.ThrownException = ex;
         }
+    }
+
+    [When("I call ChuckNorrisShuffle")]
+    public void WhenICallChuckNorrisShuffle()
+    {
+        context.ShuffledList = context.List.ChuckNorrisShuffle();
+    }
+
+    [When("I call SurviveChuckNorris on the nullable list")]
+    public void WhenICallSurviveChuckNorrisOnTheNullableList()
+    {
+        context.FilteredList = context.NullableList.SurviveChuckNorris();
+    }
+
+    [When("I call ChuckNorrisCount")]
+    public void WhenICallChuckNorrisCount()
+    {
+        context.CountResult = context.List.ChuckNorrisCount();
     }
 
     [Then("the list should be empty")]
@@ -74,5 +104,35 @@ public class CollectionExtensionsSteps(CollectionExtensionsContext context)
     public void ThenAnInvalidOperationExceptionShouldBeThrown()
     {
         context.ThrownException.Should().BeOfType<InvalidOperationException>();
+    }
+
+    [Then("the shuffled list should contain all original items")]
+    public void ThenTheShuffledListShouldContainAllOriginalItems()
+    {
+        context.ShuffledList.Should().BeEquivalentTo(context.OriginalList);
+    }
+
+    [Then("the shuffled list should contain {string}")]
+    public void ThenTheShuffledListShouldContain(string expected)
+    {
+        context.ShuffledList.Should().Contain(expected);
+    }
+
+    [Then("the filtered list should contain {string}, {string}, {string}")]
+    public void ThenTheFilteredListShouldContain(string v1, string v2, string v3)
+    {
+        context.FilteredList.Should().BeEquivalentTo([v1, v2, v3]);
+    }
+
+    [Then("the filtered list should have {int} elements")]
+    public void ThenTheFilteredListShouldHaveElements(int count)
+    {
+        context.FilteredList.Should().HaveCount(count);
+    }
+
+    [Then("the count result should be {int}")]
+    public void ThenTheCountResultShouldBe(int expected)
+    {
+        context.CountResult.Should().Be(expected);
     }
 }
